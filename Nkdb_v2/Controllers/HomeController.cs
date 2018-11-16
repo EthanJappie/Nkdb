e0029eff60e46b7573ecc82fb7844b15fc860e79
Ethan Jappie <JappieE@timesmedia.co.za>
2018-11-16 16:57:49 +0200

    revised ui

--- Nkdb_v2/Controllers/HomeController.cs
+++ Nkdb_v2/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace Nkdb_v2.Controllers
 {
+    //[RequireHttps]
     public class HomeController : Controller
     {
         public ActionResult Index()
---
+++ Nkdb_v2/DataAccess/Authentication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Data.Common;
+
+namespace Nkdb_v2.DataAccess
+{
+    //public class Authentication: DbContext
+    //{
+    //    public Authentication() : base("Authentication") { };
+    //}
+}
